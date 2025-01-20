182d5ae17e11ab96b5a986b86a95d208f30fde7b
ppsb1212 <ppsb1212@gmail.com>
2025-01-20 16:50:26 +0530

    Save data and generate HTML for delivery options

--- Amazon Project/data/cart.js
+++ Amazon Project/data/cart.js
@@ -1,12 +1,18 @@
-export let cart = JSON.parse(localStorage.getItem('cart'))  ||
-    [{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2
-    },{
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
-    }];
+export let cart = JSON.parse(localStorage.getItem('cart'));  
 
+if(!cart){
+  cart =  [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+},{
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+}];
+
+}
+   
 export function saveToStorage(){
   localStorage.setItem('cart', JSON.stringify(cart));
 }
@@ -30,7 +36,8 @@ export function addToCart(productId){
     else{
       cart.push({
         productId,
-        quantity
+        quantity,
+        deliveryOptionId: '1'
       });
       }
 
@@ -49,4 +56,23 @@ export function removeFromCart(productId){
   cart = newCart;
 
   saveToStorage();
+}
+
+export function calculateCartQuantity(){
+  let cartQuantity = 0;
+    
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity){
+  cart.forEach((cartItem) => {
+    if(cartItem.productId === productId){
+      cartItem.quantity = newQuantity;
+      saveToStorage();
+    }
+  })
 }
--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -63,14 +63,12 @@ const addedMessageTimeouts = {};
 
 
 function updateCartQuantity(){
-    let cartQuantity = 0;
-    
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
+    const cartQuantity = calculateCartQuantity();
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
+
+updateCartQuantity();
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click',() =>{
